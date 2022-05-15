ad41d1d275d16a9787824727f26446c228bf8124
zakaria <erfan.yekezare@gmail.com>
2022-05-15 19:43:21 +0430

    implement and recognize face landmarks, with faceMesh instance.

--- FaceDirection/FaceDirection.py
+++ FaceDirection/FaceDirection.py
@@ -1,6 +1,6 @@
-from traceback import print_tb
 import cv2
 import mediapipe as mp
+from numpy import result_type
 
 # Create mediapipe modules and implement classes from it.
 mpFaceMesh = mp.solutions.face_mesh
@@ -11,8 +11,8 @@ faceMesh = mpFaceMesh.FaceMesh(
 
 # Read feed from webcam and resize
 cap = cv2.VideoCapture(0)
-cap.set(3, 720)
-cap.set(4, 1080)
+cap.set(3, 1080)
+cap.set(4, 720)
 
 while True:
     isReadOk, frame = cap.read()
@@ -20,10 +20,30 @@ while True:
         print("could'nt read the feed from webcam.")
         break
     
+    # Edit input frame to be RGB
+    # and flip it to be as a selfie.
     frame = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 1)
-    
+    # make it not writeable to procces faster.
     frame.flags.writeable = False
-    cv2.imshow("Capture", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    
+    # Process the frame.
+    result = faceMesh.process(frame)
+    
+    # make it writeable.
+    frame.flags.writeable = True
+    
+    # Convert it back to BGR to be able to work on it.
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    
+    # Check if results have been aquierd then:
+    # Loop trough landmarks.
+    if result.multi_face_landmarks:
+        for faceLandmark in result.multi_face_landmarks:
+            print(faceLandmark)
+    
+    
+    #cv2.imshow("Capture", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    cv2.imshow("Capture", frame)
     
     if cv2.waitKey(5) & 0xFF == ord("q"):
         break
