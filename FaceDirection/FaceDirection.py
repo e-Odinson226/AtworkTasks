1108754a55cab10de78487f7e43f94ffca0be575
zakaria <erfan.yekezare@gmail.com>
2022-05-15 19:30:53 +0430

    create instance from mediapipe library.

--- FaceDirection/FaceDirection.py
+++ FaceDirection/FaceDirection.py
@@ -2,6 +2,14 @@ from traceback import print_tb
 import cv2
 import mediapipe as mp
 
+# Create mediapipe modules and implement classes from it.
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh( 
+                               max_num_faces=1,
+                               min_detection_confidence=0.5,
+                               min_tracking_confidence=0.5  )
+
+# Read feed from webcam and resize
 cap = cv2.VideoCapture(0)
 cap.set(3, 720)
 cap.set(4, 1080)
