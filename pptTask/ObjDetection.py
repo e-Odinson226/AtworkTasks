81ef4823102243f878f3f2496209c3492a5912b7
zakaria <erfan.yekezare@gmail.com>
2022-05-17 15:54:10 +0430

    tmp

--- pptTask/ObjDetection.py
+++ pptTask/ObjDetection.py
@@ -39,11 +39,39 @@ while(True):
     print(f'values max: {valMax}')
     
     # Implement values on the masked frame -----------------
-    masked = cv2.inRange(frameHSV, valMin, valMax, )
+    masked = cv2.inRange(frameHSV, valMin, valMax )
     cv2.imshow("masked", masked)
-    #while True
+    
+    #height, width, fra = frame.shape
+    blankImage = np.zeros(frame.shape, np.uint8)
+
+    contours = cv2.findContours(masked, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #contours = contours[0] if len(contours) == 2 else contours[1]
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
+        print(len(approx))
+        if len(approx)==5:
+            print("Blue = pentagon")
+            cv2.drawContours(blankImage,[contour],0,255,-1)
+        elif len(approx)==3:
+            print("Green = triangle")
+            cv2.drawContours(blankImage,[contour],0,(0,255,0),-1)
+        elif len(approx)==4:
+            print("Red = square")
+            cv2.drawContours(blankImage,[contour],0,(0,0,255),-1)
+        elif len(approx) == 6:
+            print("Cyan = Hexa")
+            cv2.drawContours(blankImage,[contour],0,(255,255,0),-1)
+        elif len(approx) == 8:
+            print("White = Octa")
+            cv2.drawContours(blankImage,[contour],0,(255,255,255),-1)
+        elif len(approx) > 12:
+            print("Yellow = circle")
+            cv2.drawContours(blankImage,[contour],0,(0,255,255),-1)
     
     
+    cv2.imshow("Drawed shapes", blankImage)
     
     
     if cv2.waitKey(5) & 0xFF == ord('q'):
