211d0b8bf4343d93c406bae53bb580d1a3c88996
zakaria <erfan.yekezare@gmail.com>
2022-05-22 19:34:07 +0430

    reverse and remove [Trackbar] file

--- ObjectDetection/ObjDetectionPhotoshot.py
+++ ObjectDetection/ObjDetectionPhotoshot.py
@@ -1,38 +1,51 @@
 import os
-from matplotlib.pyplot import sca
 import mediapipe as mp
 import cv2
 import numpy as np
 
-#cap = cv2.VideoCapture(0)
+def empty(tst):
+            return tst
+
+def trackbar(minval, maxval, frameSize=[520, 170]):
+    frameSize[0] = 0
+    frameSize[1] = 0
+    frame = cv2.namedWindow("FrameSetup")
+    cv2.resizeWindow("FrameSetup", frameSize[0], frameSize[1])
+    cv2.createTrackbar("HUEmin", "FrameSetup", minval['HUE'][0], minval['HUE'][1], empty)
+    cv2.createTrackbar("HUEmax", "FrameSetup", maxval['HUE'][0], maxval['HUE'][1], empty)
+    cv2.createTrackbar("SATmin", "FrameSetup", minval['SAT'][0], minval['SAT'][1], empty)
+    cv2.createTrackbar("SATmax", "FrameSetup", maxval['SAT'][0], maxval['SAT'][1], empty)
+    cv2.createTrackbar("VALmin", "FrameSetup", minval['VAL'][0], minval['VAL'][1], empty)
+    cv2.createTrackbar("VALmax", "FrameSetup", maxval['VAL'][0], maxval['VAL'][1], empty)
+
+
 cwd = os.getcwd()
-#print(cwd)
 photoshot = os.path.join(cwd, 'shapes_on_canvas.jpg')
-print(photoshot)
 frame = cv2.imread(photoshot)
+
 # resize frame
 scale = 20
 height = int(frame.shape[0] * scale / 100)
 width = int(frame.shape[1] * scale / 100)
 frame = cv2.resize(frame, (width, height), cv2.INTER_AREA)
-# Create Trackbars -----------
-def empty(tst):
-    return tst
-cv2.namedWindow("FrameSetup")
-cv2.resizeWindow("FrameSetup", 520, 170)
-cv2.createTrackbar("HUEmin", "FrameSetup", 91, 179, empty)
-cv2.createTrackbar("HUEmax", "FrameSetup", 179, 179, empty)
-cv2.createTrackbar("SATmin", "FrameSetup", 6, 255, empty)
-cv2.createTrackbar("SATmax", "FrameSetup", 75, 255, empty)
-cv2.createTrackbar("VALmin", "FrameSetup", 55, 255, empty)
-cv2.createTrackbar("VALmax", "FrameSetup", 110, 255, empty)
 
-#backgroundMin = [0, 0, 0]
-#backgroundMax = [179, 255, 110]
-while(True):
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    cv2.imshow("feed", frameHSV)
-    
+# Create Trackbars -----------
+minval = {"HUE":(0, 179),
+          "SAT":(0, 179),
+          "VAL":(0, 179)}
+maxval = {"HUE":(0, 179),
+          "SAT":(0, 179),
+          "VAL":(0, 179)}
+trackbar(minval, maxval)
+
+#------------ Frames --------------
+
+frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+framThresh = cv2.threshold(frameGray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+while True:
     # Read frames and validate reading ----------
     HUEmin = cv2.getTrackbarPos("HUEmin", "FrameSetup")
     HUEmax = cv2.getTrackbarPos("HUEmax", "FrameSetup")
@@ -42,13 +55,18 @@ while(True):
     VALmax = cv2.getTrackbarPos("VALmax", "FrameSetup")
     valMin = np.array([HUEmin, SATmin, VALmin])
     valMax = np.array([HUEmax, SATmax, VALmax])
-    print(f'values min: {valMin}')
-    print(f'values max: {valMax}')
-    
+
     # Implement values on the masked frame -----------------
-    masked = cv2.inRange(frameHSV, valMin, valMax, )
-    cv2.imshow("masked", masked)
+    masked = cv2.inRange(frameHSV, valMin, valMax)
+    cnts = cv2.findContours(masked, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    #for cnt in cnts:
+    #    approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True), True)
+    #    #print(len(approx))
+    #    cv2.drawContours(masked, [cnt], 0, (255, 0, 0), -1)
     
+    cv2.imshow("feed", frameHSV)
+    cv2.imshow("masked", masked)
+
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
-    
