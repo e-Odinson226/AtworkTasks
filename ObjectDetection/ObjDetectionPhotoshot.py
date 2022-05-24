5043cd95736b76fbdf7c76c7e4fde49bb043e511
zakaria <erfan.yekezare@gmail.com>
2022-05-24 13:37:28 +0430

    minimal value of canny treshold is set, working well.

--- ObjectDetection/ObjDetectionPhotoshot.py
+++ ObjectDetection/ObjDetectionPhotoshot.py
@@ -4,12 +4,27 @@ import cv2
 import numpy as np
 from pathlib import Path
 
+def empty(tst):
+        pass
+    
+def trackbar(minval, maxval, frameSize=[520, 170]):
+    frameSize[0] = 0
+    frameSize[1] = 0
+    frame = cv2.namedWindow("FrameSetup")
+    cv2.resizeWindow("FrameSetup", frameSize[0], frameSize[1])
+    cv2.createTrackbar("Canny t1", "FrameSetup", minval['canny'][0], minval['canny'][1], empty)
+    cv2.createTrackbar("Canny t2", "FrameSetup", maxval['canny'][0], maxval['canny'][1], empty)
+
+# Create Trackbars -----------
+minval = {"canny":(20, 254)}
+maxval = {"canny":(100, 254)}
+trackbar(minval, maxval)
 
 def detectShape(contour):
     shape = "unidentified"
     arcLength = cv2.arcLength(contour,True)
     approx = cv2.approxPolyDP(contour, 0.01*arcLength, True)    
-    print(len(approx))
+    
     if len(approx) == 3:
         shape = "triangle"
     elif len(approx) == 4:
@@ -17,19 +32,23 @@ def detectShape(contour):
         ratio = w / h
         shape = "square" if ratio > 0.95 and ratio < 1.05 else "rectangle"
     elif len(approx) == 5:
+        
         shape = "pentagon"
     elif len(approx) == 6:
+        
         shape = "hexagon"
     elif len(approx) == 8:
+        
         shape = "screw"
-    elif len(approx) > 8:
-        shape = "circle"
+    elif len(approx) == 20:
+        
+        shape = "Bracket"
     #cv2.rectangle(frame, x, y, (150,0,150), 2)
     return shape
 
 try:
     BASE_DIR = Path(__file__).resolve().parent
-    photoshot = os.path.join(BASE_DIR, 'shapes/rectangle.jpg')
+    photoshot = os.path.join(BASE_DIR, 'shapes/screw.jpg')
     print(photoshot)
     frame = cv2.imread(photoshot)
     scale = 80
@@ -43,34 +62,36 @@ except:
 #               Blurring
 #frameBlured = cv2.medianBlur(frameGray, 5)
 frameBlured = cv2.GaussianBlur(frame, (17, 17), 0)
-cv2.imshow("frameBlured", frameBlured)
 
 frameGray = cv2.cvtColor(frameBlured, cv2.COLOR_BGR2GRAY)
 
 #               Threshing
 #frameThresh = cv2.threshold(frameBlured, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-frameThresh = cv2.threshold(frameGray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+#frameThresh = cv2.threshold(frameGray, threshT1, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 #frameThresh = cv2.adaptiveThreshold(frameBlured, 255,
 #                                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
 #                                    cv2.THRESH_BINARY, 11, 2)
 
-cannyT1 = 20
-cannyT2 = 20
-frameThresh = cv2.Canny(frameThresh, cannyT1, cannyT2)
-
 kernel = np.ones((5, 5))
-frameThresh = cv2.dilate(frameThresh, kernel, iterations=2)
 
-while True:   
+while True:
+    cannyT1 = cv2.getTrackbarPos("Canny t1", "FrameSetup")
+    cannyT2 = cv2.getTrackbarPos("Canny t2", "FrameSetup")
+        
+    frameThresh = cv2.Canny(frameGray, cannyT2, cannyT2)
+    cv2.imshow("Canny", frameThresh)
+    frameDilate = cv2.dilate(frameThresh, kernel, iterations=2)
+    cv2.imshow("Dilated", frameDilate)
+    
 
     
-    contours, hierarchy = cv2.findContours( frameThresh,
+    contours, hierarchy = cv2.findContours( frameDilate,
                                         cv2.RETR_TREE,
                                         cv2.CHAIN_APPROX_SIMPLE  )
     #trackbar(minval, maxval)
     for contour in contours:
         #print(contour)
-        cv2.drawContours(frame, contours, 0, (50, 150, 150), 3)
+        cv2.drawContours(frame, [contour], 0, (20, 250, 200), 3)
         area = cv2.contourArea(contour)
         if area > 1500:
             shape = detectShape(contour)
