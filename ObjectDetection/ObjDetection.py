698c23b24bc8afcd3e964d281bad0f798f896129
zakaria <erfan.yekezare@gmail.com>
2022-05-24 18:47:39 +0430

    working ok has noise.

--- ObjectDetection/ObjDetection.py
+++ ObjectDetection/ObjDetection.py
@@ -1,111 +1,130 @@
-from inspect import getcallargs
-from itertools import count
-from matplotlib.pyplot import get
-import mediapipe as mp
+import time
 import cv2
 import numpy as np
+from pathlib import Path
 
-cap = cv2.VideoCapture(0)
+def empty(tst):
+        pass
+    
+def trackbar(minval, maxval, frameSize=[520, 170]):
+    frameSize[0] = 0
+    frameSize[1] = 0
+    frame = cv2.namedWindow("FrameSetup")
+    cv2.resizeWindow("FrameSetup", frameSize[0], frameSize[1])
+    cv2.createTrackbar("Canny t1", "FrameSetup", minval['canny'][0], minval['canny'][1], empty)
+    cv2.createTrackbar("Canny t2", "FrameSetup", maxval['canny'][0], maxval['canny'][1], empty)
 
 # Create Trackbars -----------
-def empty(tst):
-    return tst
-cv2.namedWindow("FrameSetup")
-cv2.resizeWindow("FrameSetup", 520, 170)
-cv2.createTrackbar("HUEmin", "FrameSetup", 44, 179, empty)
-cv2.createTrackbar("HUEmax", "FrameSetup", 179, 179, empty)
-cv2.createTrackbar("SATmin", "FrameSetup", 0, 255, empty)
-cv2.createTrackbar("SATmax", "FrameSetup", 255, 255, empty)
-cv2.createTrackbar("VALmin", "FrameSetup", 0, 255, empty)
-cv2.createTrackbar("VALmax", "FrameSetup", 93, 255, empty)
+minval = {"canny":(110, 254)}
+maxval = {"canny":(250, 254)}
+trackbar(minval, maxval)
 
-def showContours(img, frameContour):
-    contours, heirarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def detectShape(contour):
+    shape = "unidentified"
+    arcLength = cv2.arcLength(contour,True)
+    approx = cv2.approxPolyDP(contour, 0.01*arcLength, True)
+    #cv2.drawContours(frame, contour, 0, (20, 250, 200), 3)
+    (x, y, w, h) = cv2.boundingRect(contour)
+    #print(len(approx))
+    if len(approx) == 3:
+        shape = "triangle"
+        
+    elif len(approx) == 4:        
+        ratio = w / h
+        shape = "square" if ratio > 0.95 and ratio < 1.05 else "rectangle"
+        
+    elif len(approx) == 5:        
+        shape = "pentagon"
+        
+    elif len(approx) == 6:        
+        shape = "hexagon"
+        
+    elif len(approx) == 8:        
+        shape = "screw"
+        
+    elif len(approx) == 20:
+        shape = "Bracket"
+        
+    elif len(approx) > 8 and len(approx) != 20:
+        shape = "circle"
+        
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0,150,150), 2)
+    cv2.putText(frame,
+                f"corners: {len(approx)}",
+                (x , y-10),
+                cv2.FONT_HERSHEY_PLAIN, 1,
+                (0,0,0), 2)
+    
+    cv2.putText(frame,
+                shape,
+                (x , y+h+10),
+                cv2.FONT_HERSHEY_PLAIN, 1,
+                (0,0,0), 2)
+    return shape
+
+try:
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1080)
+    cap.set(4, 720)
+    
+except:
+    print("Can't read video frame.")
+
+
+kernel = np.ones((5, 5))
+
+while True:
+    cannyT1 = cv2.getTrackbarPos("Canny t1", "FrameSetup")
+    cannyT2 = cv2.getTrackbarPos("Canny t2", "FrameSetup")
+    
+    isReadOk, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    begin = time.time()
+    
+    # proccess frames -----------------------------------------------------------
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    #frameBlured = cv2.medianBlur(frame, 5)
+    frameBlured = cv2.GaussianBlur(frameGray, (7, 7), 0)
+
+
+    #frameThresh = cv2.threshold(frameBlured, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    #frameThreshT = cv2.threshold(frameBlured, 70, 255, cv2.THRESH_BINARY_INV )[1]
+    #frameThreshAdap = cv2.adaptiveThreshold(frameGray, 255,
+    #                                    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                                    cv2.THRESH_BINARY, 11, 2)
+    #cv2.imshow("frameThreshT", frameThreshT)
+
+    frameThresh = cv2.Canny(frameBlured, cannyT1, cannyT2)
+    cv2.imshow("frameThresh", frameThresh)
+    
+    #frameDilate = cv2.dilate(frameThresh, kernel, iterations=1)
+    #cv2.imshow("Dilated", frameDilate)
+    
+
+    # Get contours ---------------------------------------------------------
+    contours, hierarchy = cv2.findContours( frameThresh,
+                                        cv2.RETR_LIST,
+                                        cv2.CHAIN_APPROX_SIMPLE  )
+    #trackbar(minval, maxval)
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 1000:
-            peri = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.04*peri , True)
-            #cv2.drawContours(frameContour, [contour], -1, (255, 255, 0),3 )
-            print(f'length approx:{len(approx)} | area:{area}')
-            if len(approx)==5:
-                print("Blue = pentagon")
-                cv2.drawContours(frameContour,[contour],0,255,-1)
-            elif len(approx)==3:
-                print("Green = triangle")
-                cv2.drawContours(frameContour,[contour],0,(0,255,0),-1)
-            elif len(approx)==4:
-                print("Red = square")
-                cv2.drawContours(frameContour,[contour],0,(0,0,255),-1)
-            elif len(approx) == 6:
-                print("Cyan = Hexa")
-                cv2.drawContours(frameContour,[contour],0,(255,255,0),-1)
-            elif len(approx) == 8:
-                print("White = Octa")
-                cv2.drawContours(frameContour,[contour],0,(255,255,255),-1)
-            elif len(approx) > 12:
-                print("Yellow = circle")
-                cv2.drawContours(frameContour,[contour],0,(0,255,255),-1)
+        if area > 800:
+            shape = detectShape(contour)
+            #print(shape)
+        
+            
+    #images = [frame, frameThresh]
+    #titles = ['frame', 'frameThresh']
+    #for i in range(2):
+    #    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    #    plt.title(titles[i])
+    #    plt.xticks([]),plt.yticks([])
+    #plt.show()
+            
     
+    cv2.imshow("frameThresh", frameThresh)
+    cv2.imshow("feed", frame)
 
-while(True):
-    # Read frames and validate reading ----------    
-    succ, frame = cap.read()
-    if not succ:
-        print("failed to read input frames")
-        break
-    
-    maskedFrameContour = frame.copy()
-    threshFrameContour = frame.copy()
-    # Create and represent hsv version of input frames ---------
-    frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #cv2.imshow("feed", frameHSV)
-    
-    # Create and represent canny version of input frames ---------
-    frameCanny = cv2.Canny(cv2.GaussianBlur(frame,(25, 25), 5), 7, 7)
-    #cv2.imshow("Canny", frameCanny)
-    
-    # Create and represent threshed version of input frames ---------
-    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("Gray", frameGray)
-    
-    # Create and represent thresh version of input frames ---------
-    frameThresh = cv2.threshold(cv2.GaussianBlur(frameGray, (31, 31), cv2.BORDER_DEFAULT), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU )[1]
-    cv2.imshow("Thresh", frameThresh)
-    
-    
-    # Read frames and validate reading ----------
-    HUEmin = cv2.getTrackbarPos("HUEmin", "FrameSetup")
-    HUEmax = cv2.getTrackbarPos("HUEmax", "FrameSetup")
-    SATmin = cv2.getTrackbarPos("SATmin", "FrameSetup")
-    SATmax = cv2.getTrackbarPos("SATmax", "FrameSetup")
-    VALmin = cv2.getTrackbarPos("VALmin", "FrameSetup")
-    VALmax = cv2.getTrackbarPos("VALmax", "FrameSetup")
-    valMin = np.array([HUEmin, SATmin, VALmin])
-    valMax = np.array([HUEmax, SATmax, VALmax])
-    #print(f'values min: {valMin}')
-    #print(f'values max: {valMax}')
-    
-    # Implement values on the masked frame -----------------
-    frameMasked = cv2.inRange(frameHSV, valMin, valMax )
-    cv2.imshow("masked", frameMasked)
-    
-    
-    #height, width, fra = frame.shape
-    #blankImage = np.zeros(frame.shape, np.uint8)
-    showContours(frameMasked, maskedFrameContour)
-    cv2.imshow('maskedFrameContour', maskedFrameContour)
-    showContours(frameThresh, threshFrameContour)
-    cv2.imshow('threshFrameContour', threshFrameContour)
-    #showContours(blankImage, 'blankImage')
-    #showContours(frameThresh)
-    
-    # Create and represent thresh version of input frames ---------
-
-
-    #contours = contours[0] if len(contours) == 2 else contours[1]
-    #approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
-    
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
-    
