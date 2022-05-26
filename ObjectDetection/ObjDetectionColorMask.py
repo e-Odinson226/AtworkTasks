f103b1205ddc9a25501676957eaff693df8e0198
zakaria <erfan.yekezare@gmail.com>
2022-05-26 17:38:47 +0430

    Version 1.10 modes are designed and implemented, mode0: OTSU threshold. mode1: dynamic threshold value from trackbar. mode2: color masking with HUE SAT VAL values.

--- ObjectDetection/ObjDetectionColorMask.py
+++ ObjectDetection/ObjDetectionColorMask.py
@@ -4,14 +4,37 @@ import cv2
 import time
 import numpy as np
 
+# Version 1.10 --------------------------------------------------------
+# ATTENTION: modes are designed to represent 3 states of detection method in one code
+# MODE 0: threshold frames based on OTSU method.
+# MODE 1: threshold frames dynamicaly based on THRESH_BINARY_INV method and threshold value is taken from trackbar.
+# MODE 2: THERE IS NO BINARY THRESHOLD! it uses color masking and takes values(SAT, HEU, VAL) from trackbar to filter and designates the asked color.
+# --------------------------------------------------------
+
+
 def empty(tst):
         pass
 
 def trackbar(modes = [0, 1, 2, 3], frameSize=[520, 170]):
-    frame = cv2.namedWindow("Detection Mode")
-    cv2.resizeWindow("Detail tweaks", frameSize[0], frameSize[1])
-    cv2.createTrackbar("mode", "Detection Mode", modes[0], modes[3], empty)
-    cv2.createTrackbar("thresh", "Detection Mode", 0, 255, empty)
+    minval = {  "HUE":(0, 179),
+                "SAT":(0, 179),
+                "VAL":(0, 179)}
+    maxval = {  "HUE":(0, 179),
+                "SAT":(0, 179),
+                "VAL":(0, 179)}
+    
+    frame = cv2.namedWindow("Detection Tweaks")
+    cv2.resizeWindow("Detection Tweaks", frameSize[0], frameSize[1])
+    
+    cv2.createTrackbar("mode", "Detection Tweaks", modes[0], modes[2], empty)
+    cv2.createTrackbar("thresh", "Detection Tweaks", 0, 255, empty)
+    cv2.createTrackbar("HUEmin", "Detection Tweaks", minval['HUE'][0], minval['HUE'][1], empty)
+    cv2.createTrackbar("HUEmax", "Detection Tweaks", maxval['HUE'][0], maxval['HUE'][1], empty)
+    cv2.createTrackbar("SATmin", "Detection Tweaks", minval['SAT'][0], minval['SAT'][1], empty)
+    cv2.createTrackbar("SATmax", "Detection Tweaks", maxval['SAT'][0], maxval['SAT'][1], empty)
+    cv2.createTrackbar("VALmin", "Detection Tweaks", minval['VAL'][0], minval['VAL'][1], empty)
+    cv2.createTrackbar("VALmax", "Detection Tweaks", maxval['VAL'][0], maxval['VAL'][1], empty)
+    
 
 trackbar()
 
@@ -24,8 +47,8 @@ except:
     print("Can't read video frame.")
 
 while True:
-    mode = cv2.getTrackbarPos("mode", "Detail tweaks")
-    threshValue = cv2.getTrackbarPos("thresh", "Detail tweaks")
+    mode = cv2.getTrackbarPos("mode", "Detection Tweaks")
+    threshValue = cv2.getTrackbarPos("thresh", "Detection Tweaks")
     
     
     
@@ -38,23 +61,21 @@ while True:
     frameBlured = cv2.GaussianBlur(frameGray, (3, 3), 0)
     
     if mode == 0:
-        frameThresh = cv2.threshold(frameBlured, threshValue, 255,
+        frameThresh = cv2.threshold(frameBlured, 100, 255,
                                 cv2.THRESH_OTSU)[1]
         
     elif mode == 1:
-        loweBound = np.array([100, 200, 300])
-        loweBound = np.array([100, 200, 300])
         frameThresh = cv2.threshold(frameBlured, threshValue, 255,
                                 cv2.THRESH_BINARY_INV)[1]
         frameThresh = ~frameThresh
         
     elif mode == 2:
-        HUEmin = cv2.getTrackbarPos("HUEmin", "FrameSetup")
-        HUEmax = cv2.getTrackbarPos("HUEmax", "FrameSetup")
-        SATmin = cv2.getTrackbarPos("SATmin", "FrameSetup")
-        SATmax = cv2.getTrackbarPos("SATmax", "FrameSetup")
-        VALmin = cv2.getTrackbarPos("VALmin", "FrameSetup")
-        VALmax = cv2.getTrackbarPos("VALmax", "FrameSetup")
+        HUEmin = cv2.getTrackbarPos("HUEmin", "Detection Tweaks")
+        HUEmax = cv2.getTrackbarPos("HUEmax", "Detection Tweaks")
+        SATmin = cv2.getTrackbarPos("SATmin", "Detection Tweaks")
+        SATmax = cv2.getTrackbarPos("SATmax", "Detection Tweaks")
+        VALmin = cv2.getTrackbarPos("VALmin", "Detection Tweaks")
+        VALmax = cv2.getTrackbarPos("VALmax", "Detection Tweaks")
         lowerBound = np.array([HUEmin, SATmin, VALmin])
         upperBound = np.array([HUEmax, SATmax, VALmax])
         frameThresh = cv2.inRange(frame, lowerBound, upperBound)
