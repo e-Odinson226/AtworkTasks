be56568141f5b6f424920044d74846c36af0dd74
zakaria <erfan.yekezare@gmail.com>
2022-07-06 15:25:43 +0430

    test on 5 example frames, median Adaptive is appear to performe better with detailed frames, next goal: test with contour added around closed edges.

--- pptObjectPlacement/prototype.py
+++ pptObjectPlacement/prototype.py
@@ -8,27 +8,33 @@ def preprocess_frame(addr):
     return cv2.resize(frame, (720, 480), interpolation= cv2.INTER_AREA)
 
 #---------- BEGINING TO DO COMPUTING ON FRAMES
-begin = time.time()
-
-#---------- CHOOSE THRESHOLD METHOD
 #frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 def compute_frame(frame):
-    frameMedian = cv2.medianBlur(frame, 11)
-    frameMedDenoised = cv2.fastNlMeansDenoising(frameMedian, 10, 10, 7, 21)
+    median_blur = cv2.medianBlur(frame, 11)
+    median_denoised = cv2.fastNlMeansDenoising(median_blur, 10, 10, 7, 21)
 
-    frameMedTH =  cv2.threshold(frameMedian, 100, 255,
+    median_th =  cv2.threshold(median_blur, 100, 255,
                                     cv2.THRESH_OTSU)[1]
-    frameMedDenoisedTH = cv2.adaptiveThreshold(frameMedDenoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    median_adaptive_th = cv2.adaptiveThreshold(median_denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                     cv2.THRESH_BINARY, 21, 2)
-    return [frameMedTH, frameMedDenoisedTH]
+    return [median_th, median_adaptive_th]
 
-frame1 = compute_frame(preprocess_frame('./img samples/IMG_20220705_165233.jpg'))
+address_list = ['./img samples/01.jpg',
+                './img samples/02.jpg',
+                './img samples/03.jpg',
+                './img samples/04.jpg',
+                './img samples/05.jpg',]
+images = []
+for address in address_list:
+    frame = preprocess_frame(address)
+    computed_list = compute_frame(frame)
+    images.append( {'title':'Frame',    'image': frame} )
+    images.append( {'title':'Median Threshold', 'image': computed_list[0]} )
+    images.append( {'title':'Median Adaptive Threshold', 'image': computed_list[1]} )
 #---------- END OF FRAME COMPUTING PART
-images = [  {'i': frame1[0], 't':'frameMedTH'},
-            {'i': frame1[1], 't':'frameMedDenoisedTH'}]
 plt.figure(figsize=(16, 7))
 for i in range(len(images)):
-    plt.subplot(4,3,i+1),plt.imshow(images[i]['i'],'gray')
-    plt.title(images[i]['t'])
+    plt.subplot(5,3,i+1),plt.imshow(images[i]['image'],'gray')
+    plt.title(images[i]['title'])
     plt.xticks([]),plt.yticks([])
 plt.show()
