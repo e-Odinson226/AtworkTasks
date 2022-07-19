aaab995fada2114014a89ea019043ce90fd3da37
zakaria <erfan.yekezare@gmail.com>
2022-07-19 15:36:58 +0430

    mask making operations

--- pptObjectPlacement/main.py
+++ pptObjectPlacement/main.py
@@ -1,23 +1,25 @@
 from matplotlib import pyplot as plt
 import time
 import cv2
+import numpy as np
 
 def preprocess_frame(frame):
     frame = cv2.flip(frame, 1)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return cv2.resize(frame, (1920, 1080), interpolation= cv2.INTER_AREA)
+    #return cv2.resize(frame, (1920, 1080), interpolation= cv2.INTER_AREA)
+    return frame
 
 def median_adaptive_th(frame):
-    #median_denoised = cv2.fastNlMeansDenoising(frame, 7, 21)    
-    median_blur = cv2.medianBlur(frame, 7)
-    #gaus_blur = cv2.GaussianBlur(frame, (9, 9), 0)
-    median_adaptive_th = cv2.adaptiveThreshold(median_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                    cv2.THRESH_BINARY, 11, 5)
+    #blur = cv2.fastNlMeansDenoising(frame, 7, 21)    
+    blur = cv2.medianBlur(frame, 11)
+    #blur = cv2.GaussianBlur(frame, (7, 7), 13)
+    median_adaptive_th = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                    cv2.THRESH_BINARY, 11, 7)
     return median_adaptive_th
 
 def median_th(frame):
-    median_blur = cv2.medianBlur(frame, 11)
-    median_th =  cv2.threshold(median_blur, 100, 255,
+    blur = cv2.medianBlur(frame, 11)
+    median_th =  cv2.threshold(blur, 100, 255,
                                     cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
     return median_th
 
@@ -35,8 +37,8 @@ def draw_contour(frame, contours):
 
 try:
     cap = cv2.VideoCapture(0)    
-    cap.set(3, 1920)
-    cap.set(4, 1080)
+    #cap.set(3, 1920)
+    #cap.set(4, 1080)
 except:
     print("Can't read video frame.")
 
@@ -44,24 +46,35 @@ while True:
 #---------- BEGINING TO READ
     _, frame = cap.read()
     
-    # BEGINING TO DO COMPUTING ON FRAMES
+    # -- -- -- BEGINING TO DO COMPUTING ON FRAMES
     begin = time.time()
     
-    # create a copy from original frame  for illustration purposes
+    # -- -- -- create a copy from original frame  for illustration purposes
     original_frame = cv2.flip(frame.copy() ,1)
     
-    # preprocess frames (fliping, bgr2gray, resize)
+    # -- -- -- preprocess frames (fliping, bgr2gray, resize)
     frame = preprocess_frame(frame)
     
-    # blur the frame and get a threshold
-    median_threshold = median_th(frame)
+    # -- -- -- blur the frame and get a threshold
+    threshold = median_th(frame)
+    #threshold = median_adaptive_th(frame)
+    #cv2.imshow("Threshold", threshold)
     
-    # extract contours from the thresholded frame
-    cntrs = get_contours(median_threshold)
+    # -- -- -- extract contours from the thresholded frame
+    #cntrs = get_contours(median_threshold)
+    cntrs = get_contours(threshold)
     
-    # draw contours on the original frame
+    # -- -- -- draw contours on the original frame
     original_frame = draw_contour(original_frame, cntrs)    
-    print(f"contour: {cntrs} ----------\n")
+    
+    # -- -- -- loop through contours and create a mask of True(s) and Flase(s)
+    mask = np.ones(original_frame.shape[:2], dtype="uint8") * 255
+    for cont in cntrs:
+        mask = cv2.drawContours(mask, [cont], -1, 0, -1)
+       
+    cv2.imshow("mask", mask)
+    
+    
     
 #---------- END OF FRAME COMPUTING PART
     end = time.time()
