fcf5c29fe0b08af35ab8e85c6cb1dff77623ffa0
zakaria <erfan.yekezare@gmail.com>
2022-07-23 17:58:06 +0430

    changes done to main file.

--- pptObjectPlacement/main.py
+++ pptObjectPlacement/main.py
@@ -3,18 +3,19 @@ import time
 import cv2
 import numpy as np
 
+
 def preprocess_frame(frame):
     frame = cv2.flip(frame, 1)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #return cv2.resize(frame, (1920, 1080), interpolation= cv2.INTER_AREA)
+
     return frame
 
 def median_adaptive_th(frame):
     #blur = cv2.fastNlMeansDenoising(frame, 7, 21)    
-    blur = cv2.medianBlur(frame, 11)
+    blur = cv2.medianBlur(frame, 13)
     #blur = cv2.GaussianBlur(frame, (7, 7), 13)
     median_adaptive_th = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                    cv2.THRESH_BINARY, 11, 7)
+                                    cv2.THRESH_BINARY, 17, 7)
     return median_adaptive_th
 
 def median_th(frame):
@@ -24,16 +25,28 @@ def median_th(frame):
     return median_th
 
 def get_contours(frame):
-    contours, _ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-def draw_contour(frame, contours):
+def draw_rectangle(frame, contours):
+    error_persent = 1.30
     for cont in contours:
-        cv2.drawContours(frame, cont, -1, (255, 0, 255), 2)
+        #cv2.drawContours(frame, cont, -1, (255, 0, 255), 2)
         (x,y,w,h) = cv2.boundingRect(cont)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (10,0,10), 2)
+        w = int(w * error_persent)
+        h = int(h * error_persent)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,255), 2)
     return frame
 
+def create_mask(mask, contours):
+    # Set a +1.3 coefficient as predicted error
+    error_persent = 1.30
+    for cont in contours:
+        (x,y,w,h) = cv2.boundingRect(cont)
+        w = int(w * error_persent)
+        h = int(h * error_persent)
+        cv2.rectangle(mask, (x, y), (x+w, y+h), 0, -1)
+    return mask
 
 try:
     cap = cv2.VideoCapture(0)    
@@ -45,7 +58,7 @@ except:
 while True:
 #---------- BEGINING TO READ
     _, frame = cap.read()
-    
+    #frame = cv2.resize(frame, (720, 640), interpolation= cv2.INTER_AREA)
     # -- -- -- BEGINING TO DO COMPUTING ON FRAMES
     begin = time.time()
     
@@ -65,21 +78,22 @@ while True:
     cntrs = get_contours(threshold)
     
     # -- -- -- draw contours on the original frame
-    original_frame = draw_contour(original_frame, cntrs)    
+    original_frame = draw_rectangle(original_frame, cntrs)    
     
     # -- -- -- loop through contours and create a mask of True(s) and Flase(s)
-    mask = np.ones(original_frame.shape[:2], dtype="uint8") * 255
-    for cont in cntrs:
-        mask = cv2.drawContours(mask, [cont], -1, 0, -1)
+    mask = np.ones(frame.shape[:2], dtype="uint8") * 255
+    mask = create_mask(mask, cntrs)
+    
+    masked_frame = cv2.bitwise_and(frame, frame, mask=mask)
        
-    cv2.imshow("mask", mask)
-    
-    
     
 #---------- END OF FRAME COMPUTING PART
     end = time.time()
     fps = 1 /(end-begin)
-    cv2.putText(original_frame, f"fps:{int(fps)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (20,20,20), 2)
-    cv2.imshow("feed", original_frame)    
+    cv2.putText(original_frame, f"fps:{int(fps)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,20,255), 2)
+    cv2.imshow("mask", mask)
+    cv2.imshow("Original", original_frame)
+    cv2.imshow("masked_frame", masked_frame)
+    
     if cv2.waitKey(5) & 0xFF == ord('q'):
         break
