713bde89061f50f113c5e02337392b4acfb1aa6b
zakaria <erfan.yekezare@gmail.com>
2023-02-18 17:39:50 +0330

    minor edit

--- BackgroundDetection/test.py
+++ BackgroundDetection/test.py
@@ -2,8 +2,9 @@ import numpy as np
 import cv2
 
 
-frame = cv2.imread("../test_images/Axis_4.jpg")
-cap = cv2.VideoCapture(0)
+# frame = cv2.imread("../test_images/Axis_4.jpg")
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("video.avi")
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while 1:
@@ -11,8 +12,8 @@ while 1:
     fgmask = fgbg.apply(frame)
 
     cv2.imshow("frame", fgmask)
-    k = cv2.waitKey(30) & 0xFF
-    if k == 27:
+
+    if cv2.waitKey(30) & 0xFF == 27:
         break
 
 
