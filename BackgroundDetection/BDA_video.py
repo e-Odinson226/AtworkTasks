f969ade31d229057e3efc5322069e819d790356e
Zakaria <erfan.yekezare@gmail.com>
2023-03-02 21:55:19 +0330

    develop a function based on gsoc algorithm

--- BackgroundDetection/BDA_video.py
+++ BackgroundDetection/BDA_video.py
@@ -3,28 +3,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from tools import show_image_list
 
-# def gsoc(self):
-#    cv.bgse
+
+def gsoc(frame):
+    backSub = cv2.bgsegm.createBackgroundSubtractorGSOC()
+    img = backSub.apply(frame)
+    return img
 
 
-def doG(frame, kernel_0, sigma_0, kernel_1, sigma_1):
+def dog(frame, kernel_0, sigma_0, kernel_1, sigma_1):
     gaussianBlur_0 = cv2.GaussianBlur(frame, (kernel_0, kernel_0), sigma_0)
     gaussianBlur_1 = cv2.GaussianBlur(frame, (kernel_1, kernel_1), sigma_1)
     return gaussianBlur_0 - gaussianBlur_1
 
 
-def moG2(self, frame):
+def mog(self, frame):
     img = self.backGroundSubMOG2.apply(frame)
     return img
 
 
-def kNN(self, frame):
+def knn(self, frame):
     img = self.backGroundSubKNN.apply(frame)
     return img
-
-
-def gMG(self, frame):
-    pass
 
 
 def get_frame(cap):
@@ -34,11 +33,11 @@ def get_frame(cap):
 
 
 if __name__ == "__main__":
-
     cap = cv2.VideoCapture("video.avi")
 
-    bgsMOG2 = cv2.createBackgroundSubtractorMOG2()
-    bgsKNN = cv2.createBackgroundSubtractorKNN()
+    bgs_mog2 = cv2.createBackgroundSubtractorMOG2()
+    bgs_knn = cv2.createBackgroundSubtractorKNN()
+    bgs_gsoc = cv2.bgsegm.createBackgroundSubtractorGSOC()
 
     # ----------------------------------------
     ax0 = plt.subplot(2, 2, 1)
@@ -52,15 +51,15 @@ if __name__ == "__main__":
 
     # ----------------------------------------
     im0 = ax0.imshow(get_frame(cap))
-    im1 = ax1.imshow(doG(get_frame(cap), 7, 7, 17, 13))
-    im2 = ax2.imshow(bgsMOG2.apply(get_frame(cap)))
-    im3 = ax3.imshow(bgsKNN.apply(get_frame(cap)))
+    im1 = ax1.imshow(bgs_mog2.apply(get_frame(cap), 7, 7, 17, 13))
+    im2 = ax2.imshow(bgs_knn.apply(get_frame(cap)))
+    im3 = ax3.imshow(bgs_gsoc.apply(get_frame(cap)))
 
     def update(i):
         im0.set_data(get_frame(cap))
-        im1.set_data(doG(get_frame(cap), 7, 7, 17, 13))
-        im2.set_data(bgsMOG2.apply(get_frame(cap)))
-        im3.set_data(bgsKNN.apply(get_frame(cap)))
+        im1.set_data(bgs_mog2(get_frame(cap), 7, 7, 17, 13))
+        im2.set_data(bgs_knn.apply(get_frame(cap)))
+        im3.set_data(bgs_gsoc.apply(get_frame(cap)))
 
     ani = FuncAnimation(plt.gcf(), update, interval=1)
     plt.show()
