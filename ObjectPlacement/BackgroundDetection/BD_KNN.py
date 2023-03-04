26e22295d4f81d38567a8b08b639c28698e9e798
Zakaria <erfan.yekezare@gmail.com>
2023-03-04 15:38:39 +0330

    wrap draw code in one function

--- ObjectPlacement/BackgroundDetection/BD_KNN.py
+++ ObjectPlacement/BackgroundDetection/BD_KNN.py
@@ -4,8 +4,34 @@ import cv2 as cv
 
 # frame = cv.imread("../test_images/Axis_4.jpg")
 
+
+def draw_rect_for_contours(frame, contours, hierarchy):
+    try:
+        hierarchy = hierarchy[0]
+    except:
+        hierarchy = []
+
+    height, width = foreground_mask.shape
+    min_x, min_y = width, height
+    max_x = max_y = 0
+
+    # computes the bounding box for the contour, and draws it on the frame,
+    for contour, hier in zip(contours, hierarchy):
+        (x, y, w, h) = cv.boundingRect(contour)
+        min_x, max_x = min(x, min_x), max(x + w, max_x)
+        min_y, max_y = min(y, min_y), max(y + h, max_y)
+        if w > 80 and h > 80:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    if max_x - min_x > 0 and max_y - min_y > 0:
+        cv.rectangle(frame, (min_x, min_y), (max_x, max_y), (255, 0, 0), 2)
+
+    return frame
+
+
 cap = cv.VideoCapture(
-    "/home/zakaria/Documents/Projects/AtworkTasks/dataset/video/color/video.avi"
+    # "/home/zakaria/Documents/Projects/AtworkTasks/dataset/video/color/video.avi"
+    "/home/zakaria/Documents/Projects/AtworkTasks/dataset/video/color/Bearing.avi"
 )
 
 fgbg = cv.createBackgroundSubtractorKNN()
@@ -21,29 +47,15 @@ while True:
 
     # detect obj and draw contour
 
-    contours_0, hierarchy_0 = cv.findContours(
+    contours, hierarchy = cv.findContours(
         foreground_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
     )
 
-    try:
-        hierarchy_0 = hierarchy_0[0]
-    except:
-        hierarchy_0 = []
-
-    height, width = foreground_mask.shape
-    min_x, min_y = width, height
-    max_x = max_y = 0
-
-    # computes the bounding box for the contour, and draws it on the frame,
-    for contour, hier in zip(contours_0, hierarchy_0):
-        (x, y, w, h) = cv.boundingRect(contour)
-        min_x, max_x = min(x, min_x), max(x + w, max_x)
-        min_y, max_y = min(y, min_y), max(y + h, max_y)
-        if w > 80 and h > 80:
-            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    if max_x - min_x > 0 and max_y - min_y > 0:
-        cv.rectangle(frame, (min_x, min_y), (max_x, max_y), (255, 0, 0), 2)
+    draw_rect_for_contours(
+        frame,
+        contours,
+        hierarchy,
+    )
 
     cv.imshow("frame", frame)
 
